cbbcd5ce03b7f776a4a4d0ce194a403b22a64be4
Rafael Vicio <rafaelvicio@icloud.com>
2018-11-23 01:48:48 -0200

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude })
       },
       err => this.setState({ errorMessage: err.message})
     )
-
   }
 
   render() {
@@ -23,7 +21,7 @@ class App extends Component {
     }
 
     if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month <9 ){
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season == 'winter' ? 'Burr, it is chilly' : 'Lets hi the beach'
+
+    console.log(season)
+
+    return (
+    <div>
+        <h1>{text}</h1>
+    </div>
+    )
+}
+
+export default SeasonDisplay;
